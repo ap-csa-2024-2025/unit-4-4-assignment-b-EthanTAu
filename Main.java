664d484da0fa7e204c5a56a7f0bcae284f125121
EthanTAu <etau100@student.hbuhsd.edu>
2024-11-14 19:19:20 +0000

    finsihed

--- Main.java
+++ Main.java
@@ -5,11 +5,20 @@ public class Main
   public static void main(String[] args)
   {
     // Put your solutions below
-  }
+    countSub("gida gade geda gedi ooo ive been here a long time ago", "da");
+    
 
-  public static int countSub(String word, String target)
+  public static int countSub(String sentence, String target)
   {
-    return 0;
+    int count = 0;
+
+    for (int i = 0; i < sentence.length()-(target.length() -1); i++) {
+      String L = sentence.substring(i, i+(target.length()));
+      if (L.equals(target)) {
+        count++;
+      }
+    }
+    return System.out.println("The word " + target + " appeared " + count + " times.");
   }
 
   public static int countProperContains(String word, String target)
